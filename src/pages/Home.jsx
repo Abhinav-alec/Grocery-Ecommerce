3191e4ebcbc608658d9e9daf56904a219d7b5b8a
Abhinav kumar jha <abhinav234765@gmail.com>
2025-06-02 19:02:36 +0530

    Banner

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import MainBanner from "../components/MainBanner.jsx";
+
+const Home = () => {
+  return (
+    <div className="mt-10 ">
+      <MainBanner />
+    </div>
+  );
+};
+export default Home;
