862b7c1c7d33ab15015583a94a2edcb54fea2fdc
Abhinav kumar jha <abhinav234765@gmail.com>
2025-06-05 23:17:09 +0530

    Footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
-import { useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div className="App">
-      {isSellerPath ? null : <Navbar />}
+      {!isSellerPath && <Navbar />}
       <Toaster />
       <div
-        className={'${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}'}
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };
+
 export default App;
